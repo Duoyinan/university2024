a9119f43956ee7265cd25f383f9cf7049bc0fce4
Duoyinan <student28wang@outlook.com>
2024-09-19 17:38:45 +0800

    yeah

---
+++ Homework9.18.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(void) /* this prints the number of weeks in a years */
+{
+  int s = 56;
+  
+  printf("There are %d weeks in a year.",s);
+
+
+ int words = 3020 , lines = 350;
+ printf(" There were %d words and %d lines",words,lines);
+
+
+  return 0;
+}
